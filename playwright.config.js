b4357d45d1a58914b95e6c9ea97ec8fd4b3cc2d8
nikhithabw <Nikhitha@brainwavesdigital.com>
2025-08-17 16:33:41 +0530

    Initial commit with Playwright tests

--- playwright.config.js
+++ playwright.config.js
@@ -15,6 +15,7 @@ import { on } from 'events';
  */
 export default defineConfig({
   testDir: './tests',
+  timeout: 30000,
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -24,17 +25,18 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [ ['html', { outputFolder: 'playwright-report' }] ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
-
+    headless: true,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'off',
-    //screenshot: 'on',
-    //video:'off'
+    screenshot: 'on',
+    video:'retain-on-failure'
   },
+  
 
   /* Configure projects for major browsers */
   projects: [
@@ -52,6 +54,8 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
+
+    
 
     /* Test against mobile viewports. */
     // {
